82403f73b8a01be77b06dcb450ec0cdc2d7b9625
Rodrigo Martinez Tanchela <rmtanchela@gmail.com>
2023-08-12 00:21:40 -0300

    implementando cors

--- src/main/java/br/com/rodrigo/controller/PersonController.java
+++ src/main/java/br/com/rodrigo/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,6 +54,7 @@ public class PersonController {
 		return service.findAll();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(value = "/{id}",
 		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
 	@Operation(summary = "Finds a Person", description = "Finds a Person",
@@ -72,6 +74,7 @@ public class PersonController {
 		return service.findById(id);
 	}
 	
+	@CrossOrigin(origins = {"http://localhost:8080", "https://rodrigo.com.br"})
 	@PostMapping(
 		consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  },
 		produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
--- src/test/java/br/com/rodrigo/integrationtests/swagger/SwaggerIntegrationTest.java
+++ src/test/java/br/com/rodrigo/integrationtests/swagger/SwaggerIntegrationTest.java
@@ -4,10 +4,12 @@ import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 
 import br.com.rodrigo.configs.TestConfigs;
 import br.com.rodrigo.integrationtests.testcontainers.AbstractIntegrationTest;
 
+//@CrossOrigin
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SwaggerIntegrationTest extends AbstractIntegrationTest {
 	
